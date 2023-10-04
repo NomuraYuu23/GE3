a7cef611aa2a8008e2c3ebd0d60ee4e3525fbfc9
野村優羽 <@MSI>
2023-10-04 10:59:48 +0900

    ベースキャラクター作成途中

---
+++ BaseCharacter.cpp
@@ -0,0 +1,19 @@
+#include "BaseCharacter.h"
+
+void BaseCharacter::Initialize(const std::vector<Model*>& models)
+{
+
+
+}
+
+void BaseCharacter::Update()
+{
+
+
+}
+
+void BaseCharacter::Draw(const ViewProjection& viewProjection)
+{
+
+
+}
