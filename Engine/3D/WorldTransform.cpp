bd1d7a6689e3386a22a86ba7e8a51de1f3a9896e
野村優羽 <@MSI>
2023-11-07 17:42:08 +0900

    方向変数取得

--- Engine/3D/WorldTransform.cpp
+++ Engine/3D/WorldTransform.cpp
@@ -39,7 +39,7 @@ void WorldTransform::UpdateMatrix() {
 	// 回転行列作るか
 	if (!usedRotateMatrix_) {
 		// 回転行列
-		rotateMatrix_ = matrix4x4Calc->MakeRotateXYZMatrix(transform_.rotate);
+		rotateMatrix_ = matrix4x4Calc->DirectionToDirection(Vector3{ 0.0f,0.0f,1.0f }, direction_);
 	}
 
 	//平行移動行列
