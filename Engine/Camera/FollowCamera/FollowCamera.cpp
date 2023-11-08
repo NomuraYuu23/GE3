3b5ddf8295cec5fb8dfca02285690220e4db867a
野村優羽 <@MSI>
2023-11-08 13:02:43 +0900

    カメラ座標補間

--- Engine/Camera/FollowCamera/FollowCamera.cpp
+++ Engine/Camera/FollowCamera/FollowCamera.cpp
@@ -2,6 +2,7 @@
 #include "../../Math/Vector3.h"
 #include "../../Math/Matrix4x4.h"
 #include "../../Input/input.h"
+#include "../../Math/Ease.h"
 
 void FollowCamera::Initialize() {
 
@@ -32,23 +33,14 @@ void FollowCamera::Update() {
 
 	//追従対象がいれば
 	if (target_) {
-		//追従対象からカメラまでのオフセット
-		Vector3 offset = { 0.0f, 5.0f, -50.0f };
-
-		//カメラの角度から回転行列を計算する
-		Matrix4x4 rotateMatrixX = m4Calc->MakeRotateXMatrix(viewProjection_.transform_.rotate.x);
-		Matrix4x4 rotateMatrixY = m4Calc->MakeRotateYMatrix(viewProjection_.transform_.rotate.y);
-		Matrix4x4 rotateMatrixZ = m4Calc->MakeRotateZMatrix(viewProjection_.transform_.rotate.z);
-
-		Matrix4x4 rotateMatrix = m4Calc->Multiply(
-			rotateMatrixX, m4Calc->Multiply(rotateMatrixY, rotateMatrixZ));
-
-		//オフセットをカメラの回転に合わせて回転させる
-		offset = m4Calc->TransformNormal(offset, rotateMatrix);
-
-		//座標をコピーしてオフセット分ずらす
+		// 追従座標の補間
 		Vector3 targetPos = { target_->worldMatrix_.m[3][0], target_->worldMatrix_.m[3][1], target_->worldMatrix_.m[3][2] };
-		viewProjection_.transform_.translate = v3Calc->Add(targetPos, offset);
+		interTarget_ = Ease::Easing(Ease::EaseName::Lerp, interTarget_, targetPos, rate_);
+
+		// オフセット
+		Vector3 offset = OffsetCalc();
+
+		viewProjection_.transform_.translate = v3Calc->Add(interTarget_, offset);
 
 	}
 
@@ -57,5 +49,54 @@ void FollowCamera::Update() {
 
 	//ビュー更新
 	viewProjection_.UpdateMatrix();
+
+}
+
+void FollowCamera::Reset()
+{
+
+	Vector3Calc* v3Calc = Vector3Calc::GetInstance();
+
+	// 追従対象がいれば
+	if (target_) {
+		// 追従対象・角度の初期化
+		//座標をコピーしてオフセット分ずらす
+		interTarget_ = { target_->worldMatrix_.m[3][0], target_->worldMatrix_.m[3][1], target_->worldMatrix_.m[3][2] };
+		viewProjection_.transform_.rotate.y = target_->transform_.rotate.y;
+	}
+	destinationAngleY_ = viewProjection_.transform_.rotate.y;
+
+	// 追従対象からのオフセット
+	Vector3 offset = OffsetCalc();
+	viewProjection_.transform_.translate = v3Calc->Add(interTarget_,offset);
+
+}
+
+void FollowCamera::SetTarget(const WorldTransform* target)
+{
+	target_ = target;
+	Reset();
+}
+
+Vector3 FollowCamera::OffsetCalc() const
+{
+
+	Matrix4x4Calc* m4Calc = Matrix4x4Calc::GetInstance();
+
+	//追従対象からカメラまでのオフセット
+	Vector3 offset = { 0.0f, 5.0f, -50.0f };
+
+	//カメラの角度から回転行列を計算する
+	Matrix4x4 rotateMatrixX = m4Calc->MakeRotateXMatrix(viewProjection_.transform_.rotate.x);
+	Matrix4x4 rotateMatrixY = m4Calc->MakeRotateYMatrix(viewProjection_.transform_.rotate.y);
+	Matrix4x4 rotateMatrixZ = m4Calc->MakeRotateZMatrix(viewProjection_.transform_.rotate.z);
+
+	Matrix4x4 rotateMatrix = m4Calc->Multiply(
+		rotateMatrixX, m4Calc->Multiply(rotateMatrixY, rotateMatrixZ));
+
+	//オフセットをカメラの回転に合わせて回転させる
+	offset = m4Calc->TransformNormal(offset, rotateMatrix);
+
+	return offset;
 
 }
--- Engine/Camera/FollowCamera/FollowCamera.h
+++ Engine/Camera/FollowCamera/FollowCamera.h
@@ -17,10 +17,15 @@ public:
 	void Update();
 
 	/// <summary>
+	/// リセット
+	/// </summary>
+	void Reset();
+
+	/// <summary>
 	/// 追従対象セッター
 	/// </summary>
 	/// <param name="target"></param>
-	void SetTarget(const WorldTransform* target) { target_ = target; }
+	void SetTarget(const WorldTransform* target);
 
 	/// <summary>
 	/// ビュープロジェクションゲッター
@@ -34,6 +39,11 @@ public:
 	/// <returns></returns>
 	ViewProjection* GetViewProjectionAddress() { return &viewProjection_; }
 
+private: // メンバ関数
+
+	// 追従対象からのオフセットを計算する
+	Vector3 OffsetCalc() const;
+
 private:
 
 	//ビュープロジェクション
@@ -41,6 +51,13 @@ private:
 
 	//追従対象
 	const WorldTransform* target_ = nullptr;
+
+	// 追従対象の残像座標
+	Vector3 interTarget_ = {};
+
+	float destinationAngleY_ = 0.0f;
+
+	float rate_ = 0.1f;
 
 };
 
