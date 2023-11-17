05a83dcead0ece3b137ac84821af254decc843ae
野村優羽 <@MSI>
2023-11-17 22:24:55 +0900

    パイプライン修正

--- Engine/base/GraphicsPipelineState.cpp
+++ Engine/base/GraphicsPipelineState.cpp
@@ -239,7 +239,7 @@ void GraphicsPipelineState::CreateForModel(ID3D12Device* sDevice)
 	//ResiterzerStateの設定
 	D3D12_RASTERIZER_DESC rasterizerDesc{};
 	//裏面(時計回り)を表示しない
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_NONE;
 	//三角形の中を塗りつぶす
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
@@ -401,7 +401,7 @@ void GraphicsPipelineState::CreateForSprite(ID3D12Device* sDevice)
 	//ResiterzerStateの設定
 	D3D12_RASTERIZER_DESC rasterizerDesc{};
 	//背面も映す
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_NONE;
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
 	//三角形の中を塗りつぶす
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
